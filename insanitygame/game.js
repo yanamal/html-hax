52254d63aa320adc758786c4ec9e453fb87431b9
kennotbarbie <35541451+kennotbarbie@users.noreply.github.com>
2018-03-13 16:19:33 -0500

    kennedynewrule

--- insanitygame/game.js
+++ insanitygame/game.js
@@ -2,6 +2,12 @@ var score = 0;
 
 function updateGame() {
   $('#s').text(score);
+  // Put code for activating/deactivating your rule here:
+  if( score >= 10 ) {
+    $('#fivepointsbutton').css('display', 'inline');
+  }
+
+}
 
   // Put code for activating/deactivating your rule here:
 function insanityRule() {
---
+++ insanitygame/kennedysinsanerule.js
@@ -0,0 +1,8 @@
+function insanityRule() {
+    if (score%2 === 2) {
+      score = score * 2;
+    }
+    $('#interactive').append('<button>Click me!</button>');
+    updateGame();
+
+}
