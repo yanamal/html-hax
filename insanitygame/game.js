a85d8b366fc59c4839070a77659bd6830bc40a1c
kennotbarbie <35541451+kennotbarbie@users.noreply.github.com>
2018-03-13 16:07:15 -0500

    testing insanity puzzle

--- insanitygame/game.js
+++ insanitygame/game.js
@@ -4,6 +4,12 @@ function updateGame() {
   $('#s').text(score);
 
   // Put code for activating/deactivating your rule here:
+function insanityRule() {
+    if (score%2 === 0) {
+      score = score x 2;
+    }
+    $('#interactive').append('<button>try me!</button>');
+    updateGame();
   if( score >= 10 ) {
     $('#fivepointsbutton').css('display', 'inline');
   }
