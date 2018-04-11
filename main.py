cb84f005a0db21caad2b3b3efa71e303a62bfe6a
Yana Malysheva <kuziavra@gmail.com>
2018-04-11 14:14:23 -0500

    refactor

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 #!/usr/bin/env python
 
+# Import python libraries:
 import logging, os
+import json
+import random
 
+# Import third-party libraries:
 from google.appengine.api import users
+from flask import Flask,redirect,request,render_template
+
+# Import our own files:
 from profile import UserProfile
-from nextpuzzle import nextPuzzle
+from nextpuzzle import nextPuzzle,progress
 
-from flask import Flask,redirect
-
+# make the flask app:
 app = Flask(__name__)
 
+# Set up debug messages, when not in "real world" production mode
 production_environment = os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')
 if not production_environment:
     app.debug = True
     logging.info('debugging!')
 
-# TODO: handle all flask things in main?..
-
+# When user goes to home page, redirect to static welcome page
 @app.route('/')
 def home():
-  profile = UserProfile.get_by_user(users.get_current_user())
   return redirect('/resources/welcome.html')
 
-# automatically go to the next logical step for this user.
+# when user goes to /next,
+# figure out the next logical step for this user and redirect there.
 @app.route('/next')
 def nextStep():
   profile = UserProfile.get_by_user(users.get_current_user())
-  curr = profile.current_puzzle
-  return redirect(curr)
+  return redirect(profile.current_puzzle)
+
+# when user navigates to an autopass puzzle, either display the puzzle,
+# or (if this is a correct solution) move on to the next puzzle
+@app.route('/autopass/<puzzle>')
+def render_autopass_puzzle(puzzle):
+  # get passphrase that was submitted with this request, if any:
+  submitted = request.args.get('pass')
+  # get current user's passphrase:
+  profile = UserProfile.get_by_user(users.get_current_user())
+  # see if they submitted the correct one:
+  if submitted and (submitted == profile.current_passphrase):
+    profile.solved_puzzles.append('autopass/'+puzzle)
+    profile.put()
+    value = 'correct! '
+    value += progress('autopass/'+puzzle)
+    return value
+
+  # fallthrough logic - incorrect or no passphrase submitted:
+
+  # TODO: add extra output on pass submitted, but incorrect?
+  # (but then it'd be outside of the manual HTML structure)
+
+  # generate a new passphrase:
+  passphrase = 'default'
+  with app.open_resource('data/passphrases.json') as f:
+    passphrases = json.load(f)
+    passphrase = random.choice(passphrases)
+
+  # store it in user's profile:
+  profile.current_passphrase = passphrase
+  profile.put()
+
+  return render_template('autopass/'+puzzle, passphrase=passphrase)
--- nextpuzzle.py
+++ nextpuzzle.py
@@ -5,6 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# helper function: given the name of the current puzzle,
+# decide what the next puzzle should be.
+# TODO: getNextPuzzle
 def nextPuzzle(curr):
   with app.open_resource('data/puzzleSequence.json') as f:
     puzzles = json.load(f)
@@ -17,10 +20,9 @@ def nextPuzzle(curr):
         return None
       nextp = puzzles[i+1]
     return nextp
-# getPuzzleURL, markComplete, getCurrentPuzzle?
 
 # progress user state to the next puzzle (assume current one has just been solved);
-# return link to next puzzle.
+# return link to next puzzle, if any.
 def progress(curr):
   profile = UserProfile.get_by_user(users.get_current_user())
   nextp = nextPuzzle(curr)
