6d17899d7a036efd24f29ccb0395e74b875169d2
Wydglif <wydglif@gmail.com>
2018-04-17 16:23:05 -0500

    Quiz Question Issue

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ app = Flask(__name__)
 def home():
   profile = UserProfile.get_by_user(users.get_current_user())
   return redirect(profile.current_puzzle)
+
+#Quiz Question
+def checkDocType():
+    answer = request.args.get('struct')
+    if answer == 'pass=banana':
+        return progress('resources/doctypequiz.html')
+    else:
+￼       return 'Sorry, that\'s wrong! <a href="/resources/quizquestionissue.html">Try again?</a>'
+￼
