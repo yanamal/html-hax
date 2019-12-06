849a9840b4707c57b3a2cbebbabcccd98323186b
Yana <kuziavra@gmail.com>
2019-12-05 23:14:22 -0600

    tweaks to puzzle text

--- templates/autopass/j_script.js
+++ templates/autopass/j_script.js
@@ -1,4 +1,9 @@
 function wrong(){
-  $('#function_wrong').text('goodbye');
+  $('#function_wrong').text('Nice work!');
   window.location.href = '?pass={{ passphrase }}';
+}
+
+function right() {
+  $('#function_wrong').text('Hey, you clicked me!');
+  $('body').append('<div>How about changing my onclick="right()" to be wrong()?</div>')
 }
